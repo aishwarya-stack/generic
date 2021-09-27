fe57b0de11a6cd148eb7bf88b09a1d652fa28f9d
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-09-27 10:59:15 +0530

    ADD:welcome to generic

---
+++ Generic.java
@@ -0,0 +1,16 @@
+package Generic;
+
+import java.util.*;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Generic 
+{
+	private static final Logger LOG = LogManager.getLogger("BookAddress.class");
+	Scanner sc = new Scanner(System.in);
+public static void main(String[] args)
+{
+	LOG.info("Welcome to generic class");
+}
+}
